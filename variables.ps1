e54e75ea1f65efb8ed87ebace0c214000d34f074
Lui PK <lpereiraki@my.gcu.edu>
2024-07-18 08:49:47 -0700

    variables first commit

---
+++ variables.ps1
@@ -0,0 +1,19 @@
+$myString = "Hello World"
+
+$myString
+
+$myInt = 21
+$myDouble = 3.14
+
+$myInt.GetType()
+$myDouble.GetType()
+
+$myValue = 5
+$myValue2 = 10
+$myValue3 = 3
+
+$result = $myValue + $myValue2
+$module = $myValue2 % $myValue3
+
+$result
+$module
